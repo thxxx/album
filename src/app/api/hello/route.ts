82200a510b692f5a74027427a67828550c34fea1
thxxx <zxcv05999@naver.com>
2025-07-01 21:52:52 +0900

    test api server

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -3,18 +3,6 @@ import { NextRequest, NextResponse } from "next/server";
 import { Portkey } from "portkey-ai";
 
 export async function GET(req: NextRequest) {
-  const portkey = new Portkey({
-    apiKey: "LtxtyOnxi+fOa4EEtvcel9qgrtT0",
-    dangerouslyAllowBrowser: true,
-  });
-
-  // const newVkey = await portkey.virtualKeys.create({
-  //   name: "openai-VKey",
-  //   provider: "openai",
-  //   key: "",
-  //   expires_at: "2025-07-01T21:28:24.650Z",
-  // });
-
   const tv = process.env.TEST_VAR;
 
   return NextResponse.json({ message: `GET 요청 성공!, ${tv}` });
@@ -22,5 +10,34 @@ export async function GET(req: NextRequest) {
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
-  return NextResponse.json({ message: "POST 요청 성공!", data: body });
+
+  try {
+    const portkey = new Portkey({
+      apiKey: body.key,
+      dangerouslyAllowBrowser: true,
+    });
+
+    const newVkey = await portkey.virtualKeys.create({
+      name: "openai-VKey",
+      provider: "openai",
+      key: process.env.OPENAI_KEY,
+      expires_at: "2025-07-01T21:28:24.650Z",
+    });
+
+    return NextResponse.json({
+      message: "POST 요청 성공!",
+      data: {
+        newVkey: newVkey,
+        status: 200,
+      },
+    });
+  } catch {
+    return NextResponse.json({
+      message: "POST 요청 실패!",
+      data: {
+        newVkey: "",
+        status: 400,
+      },
+    });
+  }
 }
