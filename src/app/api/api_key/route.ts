2ba639b9d31380dbd2af7f9dee90a205f4252c9f
thxxx <zxcv05999@naver.com>
2025-07-02 00:01:30 +0900

    api server use

--- src/app/api/api_key/route.ts
+++ src/app/api/api_key/route.ts
@@ -1,6 +1,44 @@
 // src/app/api/hello/route.ts
 import { NextRequest, NextResponse } from "next/server";
 import { Portkey } from "portkey-ai";
+import { initializeApp } from "firebase/app";
+import {
+  getDatabase,
+  ref,
+  query,
+  orderByChild,
+  equalTo,
+  get,
+} from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+};
+
+const getApikeyFromUid = async (uid: string) => {
+  const app = initializeApp(firebaseConfig);
+  const db = getDatabase(app);
+
+  const usersRef = ref(db, "users");
+  const q = query(usersRef, orderByChild("uid"), equalTo(uid));
+
+  const snapshot = await get(q);
+
+  if (snapshot.exists()) {
+    snapshot.forEach((childSnap) => {
+      console.log("User found:", childSnap.key, childSnap.val());
+    });
+    return snapshot.val();
+  } else {
+    return null;
+  }
+};
 
 export async function GET(req: NextRequest) {
   const tv = process.env.TEST_VAR;
@@ -10,15 +48,27 @@ export async function GET(req: NextRequest) {
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
+  if (!body.uid) {
+    return NextResponse.json({
+      message: "request failed. cannot find uid.",
+      data: {
+        newVkey: "none",
+        status: 200,
+      },
+    });
+  }
 
   // PORT_API_KEY 요청
-  // DB에서 이미 있는지 검사, 있으면 바로 반환. 없으면 만들어서 반환
+  // DB에서 UID로 이미 있는지 검사, 있으면 바로 반환. 없으면 만들어서 반환
   try {
-    if (body.uid === "abcdefg") {
+    // const apikey = getApikeyFromUid(body.uid);
+    const apiKey = null;
+
+    if (apiKey !== null) {
       return NextResponse.json({
-        message: "POST 요청 성공!",
+        message: "POST succeed!",
         data: {
-          newVkey: "none",
+          newVkey: apiKey,
           status: 200,
         },
       });
@@ -30,17 +80,17 @@ export async function POST(req: NextRequest) {
       const apiKey = await portkey.apiKeys.create({
         name: "API_KEY_NAME_0909",
         type: "organisation",
-        "sub-type": "service",
+        "sub-type": "user",
         workspace_id: process.env.WORKSPACE_ID,
         scopes: [
-          "logs.export",
-          "logs.list",
-          "logs.view",
-          "configs.create",
-          "configs.update",
-          "configs.delete",
-          "configs.read",
-          "configs.list",
+          // "logs.export",
+          // "logs.list",
+          // "logs.view",
+          // "configs.create",
+          // "configs.update",
+          // "configs.delete",
+          // "configs.read",
+          // "configs.list",
           "virtual_keys.create",
           "virtual_keys.update",
           "virtual_keys.delete",
@@ -49,6 +99,8 @@ export async function POST(req: NextRequest) {
           "virtual_keys.copy",
         ],
       });
+
+      // 그리고 이 apiKey를 firebase DB에 저장.
 
       return NextResponse.json({
         message: "POST 요청 성공!",
